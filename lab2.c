5223e76fec2291b2b4dc783a59800e1887aa727e
Vasiliu Cristian <vasiliucristian59@gmail.com>
2024-03-06 15:16:18 +0200

    subiect1

--- lab2.c
+++ lab2.c
@@ -3,18 +3,21 @@
 
 int function(int x){
 	x=x-2;
-	x=function(x);
+
+	if (x>=5)
+        	x=function(x);
+
 	return x;
 }
 
 int main(){
 	int a=30, i=0;
-	
+
 	while (a>0){
 		a=function(a);
 		i++;
 	}
 	printf("%d", i);
-	return 0;	
+	return 0;
 }
 
